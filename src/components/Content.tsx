db76b0ca019e8766859e01ecf45bcfac32079403
lovelace <147500032+love1ace@users.noreply.github.com>
2024-07-31 13:25:56 +0900

    feature: 주석 삭제했습니다 #28

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -16,27 +16,3 @@ const StyledContent = styled.div`
 	flex-direction: column;
 	overflow: auto;
 `;
-
-// prop 으로 콘텐츠 받기
-// /** @jsxImportSource @emotion/react */
-// import { css } from '@emotion/react';
-// import { ReactNode } from 'react';
-//
-// interface ContentProps {
-//   children: ReactNode;
-// }
-//
-// const Content = ({ children }: ContentProps) => (
-//   <div css={contentStyle}>
-//     {children}
-//   </div>
-// );
-//
-// export default Content;
-//
-// const contentStyle = css`
-//   flex: 1;
-//   display: flex;
-//   flex-direction: column;
-//   overflow: auto;
-// `;
