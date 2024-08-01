06eb8cd2dd099f94ec30c514a86892581303fa16
hjkwon <sirru97@naver.com>
2024-08-01 14:28:19 +0900

    design: 캘린더에 emotion css 적용 #17

--- src/components/Calendar/CalendarWeek.tsx
+++ src/components/Calendar/CalendarWeek.tsx
@@ -1,7 +1,21 @@
-import { ICalendarWeekProps } from '../../interfaces/calendar';
+import { ICalendarWeekProps } from '@/interfaces/calendar';
+import { colors } from '@/constants/colors';
+import { fontSize } from '@/constants/font';
+import styled from '@emotion/styled';
 
 const CalendarWeek = ({ weekName }: ICalendarWeekProps) => {
-	return <div>{weekName}</div>;
+	return <Container>{weekName}</Container>;
 };
 
 export default CalendarWeek;
+
+const Container = styled.span`
+	border-bottom: 1px solid ${colors.lightGray};
+	padding: 6px 0;
+	font-size: ${fontSize.xs};
+
+	&:nth-of-type(1),
+	&:nth-of-type(7) {
+		color: ${colors.gray};
+	}
+`;
--- src/components/Calendar/ControlDate.tsx
+++ src/components/Calendar/ControlDate.tsx
@@ -1,5 +1,8 @@
-import { IControlDateProps } from '../../interfaces/calendar';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { IControlDateProps } from '@/interfaces/calendar';
+import { fontSize } from '@/constants/font';
+import IconButton from '@/components/IconButton';
+import styled from '@emotion/styled';
 
 const ControlDate = ({ nowDate, setNowDate }: IControlDateProps) => {
 	const changeMonth = (month: number) => {
@@ -7,12 +10,39 @@ const ControlDate = ({ nowDate, setNowDate }: IControlDateProps) => {
 	};
 
 	return (
-		<div>
+		<Container>
 			<span>{nowDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</span>
-			<ChevronLeft onClick={() => changeMonth(-1)} />
-			<ChevronRight onClick={() => changeMonth(1)} />
-		</div>
+			<ButtonContainer>
+				<IconButton
+					IconComponent={ChevronLeft}
+					shape="line"
+					onClick={() => changeMonth(-1)}
+				/>
+				<IconButton
+					IconComponent={ChevronRight}
+					shape="line"
+					onClick={() => changeMonth(1)}
+				/>
+			</ButtonContainer>
+		</Container>
 	);
 };
 
 export default ControlDate;
+
+const Container = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 20px;
+
+	span {
+		font-size: ${fontSize.md};
+	}
+`;
+
+const ButtonContainer = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 8px;
+`;
