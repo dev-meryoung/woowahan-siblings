676ce25a94e6a48ba97e0a5cfa3cca6f720655cd
love1ace <lovelacedud@gmail.com>
2024-08-01 22:11:28 +0900

    feature: 모달 기능 조금 구현 퍼플리싱 했음  #20

--- src/components/Calendar/mockdata.ts
+++ src/components/Calendar/mockdata.ts
@@ -19,3 +19,34 @@ export const mockdata: IMockDate[] = [
 	{ userId: '11', workDate: '2024-08-13', workType: 'middle', isOfficial: true },
 	{ userId: '12', workDate: '2024-08-14', workType: 'close', isOfficial: false },
 ];
+
+export const scheduleData = [
+	{
+		workDate: '2024-08-01',
+		wage: '10,030원',
+		workTime: '미들 (12:00~17:00)',
+		breakTime: '30분',
+		memo: '',
+	},
+	{
+		workDate: '2024-07-31',
+		wage: '10,030원',
+		workTime: '오픈 (07:00~12:00)',
+		breakTime: '30분',
+		memo: '아 퇴근하고 싶다',
+	},
+	{
+		workDate: '2024-07-30',
+		wage: '10,030원',
+		workTime: '미들 (12:00~17:00)',
+		breakTime: '30분',
+		memo: '오늘 몰래 음료수를 한개 빼먹었따',
+	},
+	{
+		workDate: '2024-07-29',
+		wage: '10,030원',
+		workTime: '미들 (12:00~17:00)',
+		breakTime: '30분',
+		memo: '푸하하',
+	},
+];
