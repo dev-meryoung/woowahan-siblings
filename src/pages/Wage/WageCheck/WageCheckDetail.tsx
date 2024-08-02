b5d4f09eb06957484d423ed55b5ad9ee589a38c0
Sonseongoh <nobleser@naver.com>
2024-08-02 15:15:22 +0900

    Design: 아랫쪽 빈공간 해결도움요청 #68

--- src/pages/Wage/WageCheck/WageCheckDetail.tsx
+++ src/pages/Wage/WageCheck/WageCheckDetail.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import workHistoryData from '@/components/Wage/workHistoryData';
 import Divider from '@/components/Divider';
 import { Timestamp } from 'firebase/firestore';
+import { fontSize } from '@/constants/font';
 
 const WageCheckDetail = () => {
 	const { id } = useParams<{ id: string }>();
@@ -23,32 +24,40 @@ const WageCheckDetail = () => {
 		return `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`;
 	};
 
+	const extractWorkingTimes = (workingTimes: string) => {
+		return workingTimes.split('|')[0].trim();
+	};
+
 	return (
 		<Container>
-			<Title>급여 상세 내역</Title>
-			<DetailRow>
-				<Value>{item.workPlace}</Value>
-			</DetailRow>
-			<DetailRow>
-				<Label>{item.amount}원</Label>
-			</DetailRow>
+			<HeaderWrapper>
+				<Title>급여 상세 내역</Title>
+				<WorkPlaceRow>
+					<Value>{item.workPlace}</Value>
+				</WorkPlaceRow>
+				<WageRow>
+					<Label>{item.amount.toLocaleString()}원</Label>
+				</WageRow>
+			</HeaderWrapper>
 			<Divider />
-			<DetailRow>
-				<Label>근무일</Label>
-				<Value>{formatDate(item.date)}</Value>
-			</DetailRow>
-			<DetailRow>
-				<Label>근무 시간</Label>
-				<Value>{item.workingTimes}</Value>
-			</DetailRow>
-			<DetailRow>
-				<Label>휴게 시간</Label>
-				<Value>30분</Value>
-			</DetailRow>
-			<DetailRow>
-				<Label>시급</Label>
-				<Value>{(item.amount / 4.5).toLocaleString()}원</Value>
-			</DetailRow>
+			<DetailWrapper>
+				<DetailRow>
+					<Label>근무일</Label>
+					<Value>{formatDate(item.date)}</Value>
+				</DetailRow>
+				<DetailRow>
+					<Label>근무 시간</Label>
+					<Value>{extractWorkingTimes(item.workingTimes)}</Value>
+				</DetailRow>
+				<DetailRow>
+					<Label>휴게 시간</Label>
+					<Value>30분</Value>
+				</DetailRow>
+				<DetailRow>
+					<Label>시급</Label>
+					<Value>{(item.amount / 4.5).toLocaleString()}원</Value>
+				</DetailRow>
+			</DetailWrapper>
 		</Container>
 	);
 };
@@ -57,9 +66,23 @@ const Container = styled.div`
 	padding: 20px;
 `;
 
+const HeaderWrapper = styled.div`
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
+const DetailWrapper = styled.div`
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
 const Title = styled.h2`
-	font-size: 24px;
-	margin-bottom: 20px;
+	font-size: 32px;
+	margin-bottom: 40px;
 `;
 
 const DetailRow = styled.div`
@@ -67,13 +90,29 @@ const DetailRow = styled.div`
 	justify-content: space-between;
 	padding: 10px 0;
 `;
+const WageRow = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 10px 0;
+	margin-bottom: 20px;
+	font-size: 30px;
+	font-weight: bold;
+`;
+const WorkPlaceRow = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 10px 0;
+	font-size: ${fontSize.xl};
+`;
 
 const Label = styled.div`
-	font-weight: bold;
+	margin-bottom: 20px;
+	font-weight: 600;
 `;
 
 const Value = styled.div`
 	text-align: right;
+	font-weight: bold;
 `;
 
 export default WageCheckDetail;
