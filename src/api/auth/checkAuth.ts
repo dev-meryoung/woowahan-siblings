6f6e3f60791655e39b2f28e4ae9ebcd1c8c33c96
meryoung <dev.meryoung@gmail.com>
2024-08-03 23:00:26 +0900

    feature: Firebase API에 사용되는 공통 로직 구현 #14

--- src/api/auth/checkAuth.ts
+++ src/api/auth/checkAuth.ts
@@ -3,7 +3,7 @@ const checkAuth = () => {
 	const sessionKey = `firebase:authUser:${import.meta.env.VITE_FIREBASE_API_KEY as string}:[DEFAULT]`;
 	const isLogin = sessionStorage.getItem(sessionKey);
 
-	return isLogin ? true : false;
+	return !!isLogin;
 };
 
 export default checkAuth;
