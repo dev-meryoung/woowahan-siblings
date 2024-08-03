d53c33181983d61b8dfd5102217b56fec1886de1
meryoung <dev.meryoung@gmail.com>
2024-08-04 02:10:31 +0900

    feature: 특정 달의 공식 스케줄에 따른 근무 정보 및 급여 관련 API 구현 #82

--- src/api/auth/checkAuth.ts
+++ src/api/auth/checkAuth.ts
@@ -1,5 +1,5 @@
 // 토큰 유효성 검증 API
-const checkAuth = () => {
+const checkAuth = (): boolean => {
 	const sessionKey = `firebase:authUser:${import.meta.env.VITE_FIREBASE_API_KEY as string}:[DEFAULT]`;
 	const isLogin = sessionStorage.getItem(sessionKey);
 
