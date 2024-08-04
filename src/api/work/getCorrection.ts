8acfc7427c050bd2930b0f131e8a7633c31bcff7
meryoung <dev.meryoung@gmail.com>
2024-08-04 16:22:40 +0900

    feature: 급여 페이지 API 구현 #87

--- src/api/work/getCorrection.ts
+++ src/api/work/getCorrection.ts
@@ -1,1 +1,27 @@
+import { db } from '@/firebaseConfig';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import getUserId from '@/api/common/getUserId';
+
 // 근무 정정신청 내역 조회
+const getCorrection = async () => {
+	const q = query(collection(db, 'WorkCorrections'), where('userId', '==', getUserId()));
+	const querySnapshot = await getDocs(q);
+	const docData = querySnapshot.docs.map((doc) => doc.data());
+	const workCorrections = docData.map((obj) => {
+		const workDate = new Date(obj.workDate.seconds * 1000);
+		const reqDate = new Date(obj.requestDate.seconds * 1000);
+		const newObj = {
+			workDate: `${workDate.getUTCFullYear()}-${workDate.getUTCMonth() + 1}-${workDate.getUTCDate()}`,
+			reqDate: `${reqDate.getUTCFullYear()}-${reqDate.getUTCMonth() + 1}-${reqDate.getUTCDate()}`,
+			workingTimes: obj.workingTimes,
+			type: obj.type,
+			approveStatus: obj.approveStatus,
+			description: obj.description,
+		};
+		return newObj;
+	});
+
+	return { workCorrections };
+};
+
+export default getCorrection;
--- src/api/work/getPersonalWage.ts
+++ src/api/work/getPersonalWage.ts
@@ -1,6 +1,25 @@
-// 특정 달 개인 스케줄에 따른 예상 급여액 조회
-const getPersonalWage = async () => {
-	// 작성 예정
+import { db } from '@/firebaseConfig';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import getUserId from '@/api/common/getUserId';
+
+// 개인 스케줄에 따른 특정 달의 예상 급여액 조회
+const getPersonalWage = async (year: number, month: number) => {
+	const startOfMonth = new Date(year, month - 1, 1);
+	const endOfMonth = new Date(year, month, 0, 23, 59, 59);
+
+	const q = query(
+		collection(db, 'PersonalSchedules'),
+		where('userId', '==', getUserId()),
+		where('date', '>=', startOfMonth),
+		where('date', '<=', endOfMonth),
+	);
+	const querySnapshot = await getDocs(q);
+	const docData = querySnapshot.docs.map((doc) => doc.data());
+
+	return {
+		totalWorkHour: docData.length * 5,
+		totalWage: docData.length * 45135,
+	};
 };
 
 export default getPersonalWage;
