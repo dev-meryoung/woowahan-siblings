7450448a8a4006b7da7d1d4cea3b7f01fb37bf96
seoyoonyi <yiseoyoon@gmail.com>
2024-08-09 01:52:53 +0900

    feat: 프로필, 404, 프로필 메뉴 상세페이지 css 수정#88

--- src/components/Profile/ProfileMenu.tsx
+++ src/components/Profile/ProfileMenu.tsx
@@ -1,5 +1,5 @@
 import { colors } from '@/constants/colors';
-import { fontSize, fontWeight } from '@/constants/font';
+import { fontWeight } from '@/constants/font';
 import styled from '@emotion/styled';
 import IconButton from '../common/Button/IconButton';
 import { ChevronRight } from 'lucide-react';
@@ -18,7 +18,7 @@ const ProfileMenu = () => {
 			{menuItems.map((item, index) => (
 				<MenuItem key={index} to={item.path}>
 					{item.name}
-					<IconButton IconComponent={ChevronRight} shape="transparent" />
+					<IconButton IconComponent={ChevronRight} shape="transparent" size={34} />
 				</MenuItem>
 			))}
 		</MenuContainer>
@@ -27,20 +27,15 @@ const ProfileMenu = () => {
 
 const MenuContainer = styled.div`
 	padding: 0 20px;
-	display: flex;
-	flex-direction: column;
 `;
 const MenuItem = styled(Link)`
 	display: flex;
 	justify-content: space-between;
-	font-size: ${fontSize.xl};
-	font-weight: ${fontWeight.semiBold};
-	padding: 20px;
-	border-bottom: 1px solid ${colors.lightestGray};
+	align-items: center;
+	font-weight: ${fontWeight.medium};
+	padding: 20px 0;
+	border-bottom: 1px solid ${colors.lightGray};
 	cursor: pointer;
-	&:hover {
-		background-color: ${colors.lightestGray};
-	}
 `;
 
 export default ProfileMenu;
--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -1,16 +1,14 @@
 /** @jsxImportSource @emotion/react */
+import LoadingDuck from '@/components/Loading/LoadingDuck';
 import { colors } from '@/constants/colors';
 import { fontSize, fontWeight } from '@/constants/font';
 import styled from '@emotion/styled';
-import { Settings } from 'lucide-react';
 
 const UnderConstruction = () => {
 	return (
 		<Container>
 			<Content>
-				<IconContainer>
-					<Settings size={50} />
-				</IconContainer>
+				<LoadingDuck />
 				<Message>이 페이지는 준비 중입니다.</Message>
 				<Description>곧 업데이트 예정입니다. 조금만 기다려 주세요!</Description>
 			</Content>
@@ -21,31 +19,30 @@ const UnderConstruction = () => {
 export default UnderConstruction;
 
 const Container = styled.div`
+	margin-top: 40px;
 	display: flex;
 	justify-content: center;
-	align-items: center;
-	height: 100%;
-	/* background-color: ${colors.lightestGray}; */
-	margin-bottom: 180px;
+
+	@media (min-width: 400px) {
+		margin-top: 100px;
+	}
 `;
 
 const Content = styled.div`
-	text-align: center;
-`;
-
-const IconContainer = styled.div`
-	margin-bottom: 20px;
-	color: ${colors.primaryYellow};
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
 `;
 
 const Message = styled.div`
-	font-size: ${fontSize.xxxxl};
+	font-size: ${fontSize.xxl};
 	font-weight: ${fontWeight.bold};
 	color: ${colors.darkestGray};
 	margin-bottom: 10px;
 `;
 
 const Description = styled.div`
-	font-size: ${fontSize.lg};
 	color: ${colors.gray};
 `;
