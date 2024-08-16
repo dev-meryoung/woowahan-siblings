4461aa5efda70cbed7d7f57050442ed45f7452b4
love1ace <lovelacedud@gmail.com>
2024-08-16 12:56:37 +0900

    feature: 모달 usecallback, 선언 정리 #169

--- src/components/SheduleModal/ScheduleModal.tsx
+++ src/components/SheduleModal/ScheduleModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/stores/store';
 import { closeModal, openModal } from '@/stores/modalSlice';
@@ -47,18 +47,10 @@ const ScheduleModal: React.FC<{
 	formattedDate: string;
 }> = ({ schedules = [], selectedSchedule, formattedDate }) => {
 	const dispatch = useDispatch<AppDispatch>();
-
 	const { isOpen, content } = useSelector((state: RootState) => state.modal);
 	const today = useMemo(() => new Date().toISOString().split('T')[0], []);
 
 	const [defaultMemo, setDefaultMemo] = useState('');
-
-	useEffect(() => {
-		if (schedules.length > 0 && schedules[0].memos.length > 0) {
-			setDefaultMemo(schedules[0].memos[0]);
-		}
-	}, [schedules]);
-
 	const [formData, setFormData] = useState({
 		workDate: convertDateToServerFormat(formattedDate) || today,
 		wage: DEFAULT_DATA.wage,
@@ -68,48 +60,11 @@ const ScheduleModal: React.FC<{
 		errors: {},
 	});
 
-	const updateFormData = (field: string, value: string) => {
-		setFormData((prevData) => ({ ...prevData, [field]: value }));
-	};
-
-	const resetForm = useCallback(
-		(schedule: ISchedule | null = null) => {
-			if (schedule) {
-				const formattedDate = new Date(schedule.workDate).toISOString().split('T')[0];
-				setFormData({
-					workDate: formattedDate,
-					wage: DEFAULT_DATA.wage,
-					workTime: DEFAULT_DATA.workTime,
-					breakTime: DEFAULT_DATA.breakTime,
-					memo: '',
-					errors: {},
-				});
-			} else {
-				const formattedDate = new Date(today).toISOString().split('T')[0];
-				setFormData({
-					workDate: formattedDate,
-					wage: DEFAULT_DATA.wage,
-					workTime: DEFAULT_DATA.workTime,
-					breakTime: DEFAULT_DATA.breakTime,
-					memo: '',
-					errors: {},
-				});
-			}
-		},
-		[today],
-	);
-
-	const setFormValues = useCallback((schedule: ISchedule) => {
-		const formattedDate = new Date(schedule.workDate).toISOString().split('T')[0];
-		setFormData({
-			workDate: formattedDate,
-			wage: DEFAULT_DATA.wage,
-			workTime: schedule.workTime,
-			breakTime: DEFAULT_DATA.breakTime,
-			memo: schedule.memos[0],
-			errors: {},
-		});
-	}, []);
+	useEffect(() => {
+		if (schedules.length > 0 && schedules[0].memos.length > 0) {
+			setDefaultMemo(schedules[0].memos[0]);
+		}
+	}, [schedules]);
 
 	useEffect(() => {
 		if (!isOpen || content === 'add' || content === 'edit') {
@@ -132,19 +87,58 @@ const ScheduleModal: React.FC<{
 				setFormValues(selectedSchedule);
 			}
 		}
-	}, [isOpen, content, selectedSchedule, resetForm, setFormValues, schedules]);
+	}, [isOpen, content, selectedSchedule, schedules]);
 
-	const handleOverlayClick = useCallback(
-		(e: React.MouseEvent) => {
-			if (e.target === e.currentTarget) {
-				dispatch(closeModal());
-				resetForm();
-			}
-		},
-		[dispatch, resetForm],
-	);
+	const updateFormData = (field: string, value: string) => {
+		setFormData((prevData) => ({ ...prevData, [field]: value }));
+	};
 
-	const isFieldDisabled = useCallback((): boolean => content === 'view', [content]);
+	function resetForm(schedule: ISchedule | null = null) {
+		if (schedule) {
+			const formattedDate = new Date(schedule.workDate).toISOString().split('T')[0];
+			setFormData({
+				workDate: formattedDate,
+				wage: DEFAULT_DATA.wage,
+				workTime: DEFAULT_DATA.workTime,
+				breakTime: DEFAULT_DATA.breakTime,
+				memo: '',
+				errors: {},
+			});
+		} else {
+			const formattedDate = new Date(today).toISOString().split('T')[0];
+			setFormData({
+				workDate: formattedDate,
+				wage: DEFAULT_DATA.wage,
+				workTime: DEFAULT_DATA.workTime,
+				breakTime: DEFAULT_DATA.breakTime,
+				memo: '',
+				errors: {},
+			});
+		}
+	}
+
+	function setFormValues(schedule: ISchedule) {
+		const formattedDate = new Date(schedule.workDate).toISOString().split('T')[0];
+		setFormData({
+			workDate: formattedDate,
+			wage: DEFAULT_DATA.wage,
+			workTime: schedule.workTime,
+			breakTime: DEFAULT_DATA.breakTime,
+			memo: schedule.memos[0],
+			errors: {},
+		});
+	}
+
+	function handleOverlayClick(e: React.MouseEvent) {
+		if (e.target === e.currentTarget) {
+			dispatch(closeModal());
+			resetForm();
+		}
+	}
+
+	function isFieldDisabled(): boolean {
+		return content === 'view';
+	}
 
 	const convertWorkTimeForFirestore = (workTime: string): TWorkingTimes => {
 		switch (workTime) {
@@ -159,68 +153,65 @@ const ScheduleModal: React.FC<{
 		}
 	};
 
-	const handleAction = useCallback(
-		async (actionType: 'save' | 'update' | 'delete') => {
-			const fields = {
-				workDate: formData.workDate,
-				wage: formData.wage,
-				workTime: formData.workTime,
-				breakTime: formData.breakTime,
-				memo: formData.memo,
-			};
-			const newErrors = validateFields(fields);
-			setFormData((prevData) => ({ ...prevData, errors: newErrors }));
+	async function handleAction(actionType: 'save' | 'update' | 'delete') {
+		const fields = {
+			workDate: formData.workDate,
+			wage: formData.wage,
+			workTime: formData.workTime,
+			breakTime: formData.breakTime,
+			memo: formData.memo,
+		};
+		const newErrors = validateFields(fields);
+		setFormData((prevData) => ({ ...prevData, errors: newErrors }));
 
-			if (Object.keys(newErrors).length === 0) {
-				const workingTime: TWorkingTimes = convertWorkTimeForFirestore(formData.workTime);
-				const serverFormattedDate = convertDateToServerFormat(formData.workDate);
-				let success = false;
+		if (Object.keys(newErrors).length === 0) {
+			const workingTime: TWorkingTimes = convertWorkTimeForFirestore(formData.workTime);
+			const serverFormattedDate = convertDateToServerFormat(formData.workDate);
+			let success = false;
 
-				if (actionType === 'save') {
-					success = await createPersonalSchedule(
-						serverFormattedDate,
-						workingTime,
-						formData.memo,
-					);
-				} else if (actionType === 'update') {
-					const oldWorkingTime: TWorkingTimes = schedules[0].workTime as TWorkingTimes;
-					success = await updatePersonalSchedule(
-						serverFormattedDate,
-						oldWorkingTime,
-						workingTime,
-						formData.memo,
-					);
-				} else if (actionType === 'delete') {
-					success = await deletePersonalSchedule(serverFormattedDate, workingTime);
-				}
-
-				if (success) {
-					dispatch(
-						fetchSchedules({
-							year: new Date(serverFormattedDate).getFullYear(),
-							month: new Date(serverFormattedDate).getMonth() + 1,
-							isOfficial: false,
-						}),
-					);
-					dispatch(closeModal());
-					resetForm();
-				} else {
-					console.error(`Failed to ${actionType} personal schedule`);
-				}
+			if (actionType === 'save') {
+				success = await createPersonalSchedule(
+					serverFormattedDate,
+					workingTime,
+					formData.memo,
+				);
+			} else if (actionType === 'update') {
+				const oldWorkingTime: TWorkingTimes = schedules[0].workTime as TWorkingTimes;
+				success = await updatePersonalSchedule(
+					serverFormattedDate,
+					oldWorkingTime,
+					workingTime,
+					formData.memo,
+				);
+			} else if (actionType === 'delete') {
+				success = await deletePersonalSchedule(serverFormattedDate, workingTime);
 			}
-		},
-		[formData, schedules, dispatch, resetForm],
-	);
+
+			if (success) {
+				dispatch(
+					fetchSchedules({
+						year: new Date(serverFormattedDate).getFullYear(),
+						month: new Date(serverFormattedDate).getMonth() + 1,
+						isOfficial: false,
+					}),
+				);
+				dispatch(closeModal());
+				resetForm();
+			} else {
+				console.error(`Failed to ${actionType} personal schedule`);
+			}
+		}
+	}
 
 	const handleSave = () => handleAction('save');
 	const handleUpdate = () => handleAction('update');
 	const handleDelete = () => handleAction('delete');
 
-	const handleEdit = useCallback(() => {
+	function handleEdit() {
 		dispatch(openModal('edit'));
-	}, [dispatch]);
+	}
 
-	const getTitle = useCallback(() => {
+	function getTitle() {
 		switch (content) {
 			case 'add':
 				return '일정 추가';
@@ -231,7 +222,7 @@ const ScheduleModal: React.FC<{
 			default:
 				return '';
 		}
-	}, [content]);
+	}
 
 	return (
 		<>
