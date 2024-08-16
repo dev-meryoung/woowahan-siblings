9d22539a290ed90561166a7a433037cde929818b
seoyoonyi <yiseoyoon@gmail.com>
2024-08-17 00:31:29 +0900

    feat: getCorrection api id값 추가 #170

--- src/api/work/getCorrection.ts
+++ src/api/work/getCorrection.ts
@@ -6,11 +6,12 @@ import { getUserId } from '@/utils/userDataUtils';
 const getCorrection = async () => {
 	const q = query(collection(db, 'WorkCorrections'), where('userId', '==', getUserId()));
 	const querySnapshot = await getDocs(q);
-	const docData = querySnapshot.docs.map((doc) => doc.data());
-	const workCorrections = docData.map((obj) => {
+	const docData = querySnapshot.docs.map((doc) => {
+		const obj = doc.data();
 		const workDate = new Date(obj.workDate.seconds * 1000);
 		const reqDate = new Date(obj.requestDate.seconds * 1000);
 		const newObj = {
+			id: doc.id,
 			workDate: `${workDate.getFullYear()}-${workDate.getMonth() + 1}-${workDate.getDate()}`,
 			reqDate: `${reqDate.getFullYear()}-${reqDate.getMonth() + 1}-${reqDate.getDate()}`,
 			workingTimes: obj.workingTimes,
@@ -21,7 +22,7 @@ const getCorrection = async () => {
 		return newObj;
 	});
 
-	return { workCorrections };
+	return { workCorrections: docData };
 };
 
 export default getCorrection;
